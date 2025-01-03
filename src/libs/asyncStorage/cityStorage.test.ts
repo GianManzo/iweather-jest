22952e31479026d88a4d840bf9a5cb148d515294
Gian <ggianmanzo@gmail.com>
2025-01-03 08:58:34 -0300

    test: safeStorageCity

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,8 +1,15 @@
-import { getStorageCity } from "./cityStorage";
+import { getStorageCity, saveStorageCity } from "./cityStorage";
+import { mockCityListFormated } from "@__tests__/mocks/components/mockCityListFormated";
 
 describe("Storage: cityStorage", () => {
   it("should be return null when dont have city storage", async () => {
     const response = await getStorageCity();
     expect(response).toBeNull();
+  });
+
+  it("should be return city storaged", async () => {
+    await saveStorageCity(mockCityListFormated[0]);
+    const response = await getStorageCity();
+    expect(response).toEqual(mockCityListFormated[0]);
   });
 });
