de2c18a9fa65f523cda9fb11e450a133e08dcf2e
Gian <ggianmanzo@gmail.com>
2025-01-03 10:08:08 -0300

    test: safe area context

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,5 @@
 import "expo-modules-core";
+global.setImmediate =
+  global.setImmediate || ((fn, ...args) => setTimeout(fn, 0, ...args));
+global.clearImmediate =
+  global.clearImmediate || ((handle) => clearTimeout(handle));
